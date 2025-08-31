e180ae33245bf37f09f390e0c4cdf0427b4b7439
0404233 <andrey0404233@gmail.com>
2025-09-01 01:45:58 +0300

    fix: vite.confog file

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/react-perfomance/',
+  base: '/react-performance/',
   plugins: [react()],
   test: {
     environment: 'jsdom',
